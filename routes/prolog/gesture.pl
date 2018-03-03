476efb70fb845194c985850769fa54db93eea6d3
sohjunjie <junjie.soh93@gmail.com>
2018-03-03 12:27:36 +0800

    add default gestures

--- routes/prolog/gesture.pl
+++ routes/prolog/gesture.pl
@@ -28,6 +28,9 @@ appropriate_gesture(no_pain, weepy, X):- normal_gesture(X).
 appropriate_gesture(no_pain, stressed, X):- normal_gesture(X).
 appropriate_gesture(no_pain, confused, X):- normal_gesture(X).
 
+% default to normal gesture if above combination does not satisfy
+appropriate_gesture(Y, Z, X):- normal_gesture(X).
+
 polite_gesture([look_concerned, mellow_voice, light_touch, faint_smile]).
 calming_gesture([greet, look_composed, look_attentive]).
 normal_gesture([broad_smile, joke, beaming_voice]).
